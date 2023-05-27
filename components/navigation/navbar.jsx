e1a744b9874abca498fcd7961ff567bf8a123390
djiwandou <djiwandou.asp@gmail.com>
2023-05-27 13:30:55 +0700

    Update navbar.jsx
    add Djiwandou 2023

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,6 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
+			<h2 style={{ fontSize: "8px" }}>Djiwandou 2023</h2>
 			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
 				<img
 					className={styles.alchemy_logo}
