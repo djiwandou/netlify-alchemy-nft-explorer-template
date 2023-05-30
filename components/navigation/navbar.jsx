a3ce8524e1b9824615db3ac1e8f9c48317f84c74
djiwandou <djiwandou.asp@gmail.com>
2023-05-30 18:53:43 +0700

    Update navbar.jsx
    update time

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,7 +3,7 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<h2 style={{ fontSize: "8px" }}>deployed by: Djiwandou 28-May-2023 19.56WIB</h2>
+			<h2 style={{ fontSize: "8px" }}>deployed by: Djiwandou 30-May-2023 18.53WIB</h2>
 			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
 				<img
 					className={styles.alchemy_logo}
